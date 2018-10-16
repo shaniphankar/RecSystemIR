0945a2ee7940e2277671651b179c3961c3add56b
shaniphankar <shaniphankar@users.noreply.github.com>
2018-10-16 22:12:59 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pprint
+import os
+from random import randint
+import matrixFuncs
+
+def main():
+	pp=pprint.PrettyPrinter(indent=4)
+	mID_names={}
+	# f=open(os.getcwd()+'/ml-1m/movies.dat',encoding='latin-1')
+	# for line in f:
+	# 	mID_names[line.split('::')[0]]=line.split('::')[1]
+	# pp.pprint(mID_names)
+	# f.close()
+	mID_uID_rating=np.zeros(shape=(3952,6040))
+	f=open(os.getcwd()+'/ml-1m/ratings.dat',encoding='latin-1')
+	for line in f:
+		mID_uID_rating[int(line.split('::')[1])-1][int(line.split('::')[0])-1]=line.split('::')[2]
+	# pp.pprint(mID_uID_rating)
+	f.close()
+	C,U,R=matrixFuncs.CUR(mID_uID_rating,4)
+	# print(data)
+
+if __name__ == '__main__':
+	main()
---
+++ matrixFuncs.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pprint
+import os
+from random import choices
+def CUR(matrix,r):
+	nR=matrix.sum(axis=1)
+	nC=matrix.sum(axis=0)
+	print(nR)
+	print(nC)
+	full=matrix.sum()
+	print(full)
+	weightsR=nR/full
+	weightsC=nC/full
+	choicesR=range(3952)
+	choicesC=range(6040)
+	C=choices(choicesC,weightsC,k=r)
+	R=choices(choicesR,weightsR,k=r)
+	W=[]
+	for i in C:
+		emptyRow=[]
+		for j in R:
+			emptyRow.append(matrix[j-1][i-1])
+		W.append(emptyRow)
+	W=np.array(W)
+	print(W)
+	#Using in built SVD for now. Replace with the other group's version
+	X,Sig,Y=np.linalg.svd(W,full_matrices=False)
+	SigInv=np.linalg.pinv(np.diag(Sig))
+	print(X)
+	print(SigInv)
+	print(Y)
+	U=np.matmul(np.matmul((np.matmul(Y,SigInv)),SigInv),np.transpose(X))
+	print(U)
+	# Cmat=matrix[:,C]
+	Rmat=matrix[R]
+	Cmat=matrix[:,C]
+	return Cmat,U,Rmat
