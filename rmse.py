c5f15cad047f4e3eb808d0f272f3ce3407414e24
Aditya Addepalli <dyex719@gmail.com>
2018-10-25 20:02:59 +0530

    Added spearman and topk precision

--- rmse.py
+++ rmse.py
@@ -3,6 +3,7 @@ import pprint
 import os
 from random import randint
 import math
+from scipy.stats import spearmanr
 
 test_data = np.load("test_data.npy")
 collab_matrix = np.load("collab2.npy")
@@ -25,5 +26,68 @@ def root_mean_square_error(test_data,collab_matrix,orig_matrix):
 	rmse = math.sqrt(squared_sum/non_zero)
 	return rmse
 
-rmse = root_mean_square_error(test_data,collab_matrix,mID_uID_rating)
-print(rmse)
+def spearman_correlation_coefficient(test_data, collab_matrix,orig_matrix):
+	test_orig_matrix = []
+	test_collab_matrix = []
+	index = 0;
+	for pair in test_data:
+		if(orig_matrix[pair[0]][pair[1]] != 0):
+			test_orig_matrix.append((index,orig_matrix[pair[0]][pair[1]]))
+			test_collab_matrix.append((index,collab_matrix[pair[0]][pair[1]]))
+			index +=1
+	test_orig_matrix = sorted(test_orig_matrix,key=lambda x: x[1],reverse=True)
+	test_collab_matrix = sorted(test_collab_matrix,key=lambda x: x[1],reverse=True)
+	rank_orig = []
+	rank_collab = []
+	#print((test_orig_matrix))
+	#print(len(test_collab_matrix))
+	for i in range(len(test_orig_matrix)):
+		rank_orig.append((i,test_orig_matrix[i][0]))
+		rank_collab.append((i,test_collab_matrix[i][0]))
+	#print(rank_orig)
+	dict1 = dict(rank_orig)
+	dict2 = dict(rank_collab)
+	#print(dict1)
+	a = [dict1[key] - dict2.get(key, 0) for key in dict1.keys()]
+	#print(a)
+	sum = 0
+	for i in a:
+		sum += a[i]**2
+	n = len(a)
+	rho = 1- ((6*sum)/((n)*((n**2) - 1)))
+	print(rho)
+	#print(dict2)
+
+
+	#print(test_orig_matrix)
+	#print(test_collab_matrix)
+
+def precision_topk(test_data, collab_matrix,orig_matrix,k,threshold):
+	store = []
+	for pair in test_data:
+		print("collab_matrix had %f, test_data had %d"%(collab_matrix[pair[0]][pair[1]],orig_matrix[pair[0]][pair[1]]) )
+		if(orig_matrix[pair[0]][pair[1]] != 0):
+			store.append((collab_matrix[pair[0]][pair[1]],orig_matrix[pair[0]][pair[1]]))
+	store = sorted(store,key=lambda x: x[0],reverse=True)[:k]
+	count = 0
+	for i in store:
+		if(i[1]>threshold):
+			count+=1
+	print(count/k)
+	return store
+
+
+# rmse = root_mean_square_error(test_data,collab_matrix,mID_uID_rating)
+# print(rmse)
+
+#inbuilt = inbuilt_scc(test_data,collab_matrix,mID_uID_rating)
+#print(inbuilt)
+
+#built = spearman_correlation_coefficient(test_data,collab_matrix,mID_uID_rating)
+#print(built)
+
+#print(collab_matrix,mID_uID_rating)
+#a = precision_topk(test_data,collab_matrix,mID_uID_rating,10,2.5)
+#print(a)
+
+rho = spearman_correlation_coefficient(test_data,collab_matrix,mID_uID_rating)
